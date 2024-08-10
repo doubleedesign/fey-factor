13d58fd5361f8391e9acd04db04e6e49bd7ce8ca
Leesa Ward <leesa@doubleedesign.com.au>
2024-08-10 18:47:11 +1000

    *shrug*

--- builder/src/scripts/topup-db.ts
+++ builder/src/scripts/topup-db.ts
@@ -1,19 +1,26 @@
-import { db } from '../common.ts';
+import { customConsole, db } from '../common.ts';
 import { PopulationScriptSettings } from './types.ts';
 import { DataPopulatorCommon } from './DataPopulatorCommon.ts';
 
-class Topper extends DataPopulatorCommon {
+class TvTopper extends DataPopulatorCommon {
+	count: number;
 
-	constructor(settings: PopulationScriptSettings) {
+	constructor({ count, ...settings }: PopulationScriptSettings & { count: number }) {
 		super(settings);
+		this.count = count;
 	}
 
 	async run() {
 		await super.setup();
 
+		// TODO: Remember what I was going to do with this and do it
 	}
 }
 
 export function topupDb({ count, ...settings }: PopulationScriptSettings & { count: number }) {
-
+	new TvTopper({ count, ...settings }).run().then(() => {
+		customConsole.stopAllProgressBars();
+		customConsole.logProgress('TV show top-up complete.');
+		customConsole.success('TV show top-up complete.', true);
+	});
 }
