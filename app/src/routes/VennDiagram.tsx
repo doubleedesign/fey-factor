45d7fb1f7e1140b55be196da01cdbe5a87c8bd15
Leesa Ward <leesa@doubleedesign.com.au>
2024-09-27 11:35:05 +1000

    Start on Venn diagram

--- app/src/routes/VennDiagram.tsx
+++ app/src/routes/VennDiagram.tsx
@@ -1,9 +1,12 @@
 import { FC, Suspense } from 'react';
 import { ControlBar, PageWrapper } from '../components/layout';
 import { Heading, LeadParagraph } from '../components/typography';
+import { Container } from '../components/common.ts';
+import { VennDiagram as Venn } from 'reaviz';
 
 export const VennDiagram: FC = () => {
 
+	
 	return (
 		<Suspense>
 			<PageWrapper>
@@ -13,7 +16,17 @@ export const VennDiagram: FC = () => {
 						<LeadParagraph>The idea that started it all</LeadParagraph>
 					</div>
 				</ControlBar>
-
+				<Container>
+					<Venn
+						height={300}
+						width={300}
+						data={[
+							{ key: ['A'], data: 12 },
+							{ key: ['B'], data: 12 },
+							{ key: ['A', 'B'], data: 2 }
+						]}
+					/>
+				</Container>
 			</PageWrapper>
 		</Suspense>
 	);
