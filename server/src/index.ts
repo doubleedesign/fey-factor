f70d311252a0271814f1ec3849d8e71eb4acd30b
Leesa Ward <leesa@doubleedesign.com.au>
2025-11-02 13:46:57 +1100

    Server: Custom error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import { createYoga, createSchema } from 'graphql-yoga';
+import { GraphQLError } from 'graphql';
 import { createServer } from 'http';
 import { readFileSync } from 'fs';
 import gql from 'graphql-tag';
@@ -14,7 +15,48 @@ const schema = createSchema({
 });
 
 // Create the Yoga server
-const yoga = createYoga({ schema });
+const yoga = createYoga({
+	schema,
+	plugins: [
+		{
+			onExecute({ args, context }) {
+				return {
+					onExecuteDone(event) {
+						if (event.result.errors) {
+							event.result.errors = event.result.errors.map(error => {
+								const exclude = ['at process.processTicksAndRejections', 'node_modules\\pg-pool\\index.js'];
+								const excludeStartingWith = ['at async Promise.all (index 0)', 'at async promiseForObject'];
+								const stackLines = error.stack.split(/\r\n|\r|\n/).map(line => line.trim()).filter(line => {
+									return !exclude.some(excludedPhrase => line.includes(excludedPhrase))
+										&& !line.startsWith('error:')
+										&& !line.startsWith('at async file:')
+										&& !excludeStartingWith.some(thing => line.startsWith(thing));
+								});
+
+								console.log(stackLines);
+
+								return new GraphQLError(error.message, {
+									path: error.path,
+									extensions: {
+										stack: stackLines.map(line => {
+											const location = line.match(/\((.*):(\d+):(\d+)\)$/g);
+
+											return ({
+												function: line.replace('at async', '').split(' (')[0].trim(),
+												location: location[0]?.split('fey-factor')?.[1]?.replaceAll('\\', '/') ?? undefined
+											});
+										}),
+										code: 'CUSTOM_ERROR_HANDLER',
+									}
+								});
+							});
+						}
+					}
+				};
+			}
+		}
+	],
+});
 
 // Create an HTTP server and attach the Yoga server to it
 const server = createServer(yoga);
