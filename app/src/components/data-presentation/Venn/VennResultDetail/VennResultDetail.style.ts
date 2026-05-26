4c53345ec72c31a131f767f7426be2f30bac9f45
Leesa Ward <leesa@doubleedesign.com.au>
2026-05-26 11:17:56 +1000

    Fix(VennResultDetail): Button styling in selected intersection

--- app/src/components/data-presentation/Venn/VennResultDetail/VennResultDetail.style.ts
+++ app/src/components/data-presentation/Venn/VennResultDetail/VennResultDetail.style.ts
@@ -7,6 +7,7 @@ export const StyledVennResultDetail = styled.div`
 	padding: ${props => props.theme.spacing.sm};
 	box-shadow: ${props => `0 0 0.25rem 0 ${props.theme.colors.subtle}`};
 	border-radius: ${props => props.theme.spacing.xs};
+	margin-block-end: ${props => props.theme.spacing.sm};
 	
 	${StyledLabel} {
 		margin: 0;
@@ -23,11 +24,34 @@ export const StyledVennResultDetailHeader = styled.div`
 		font-size: ${props => props.theme.fontSizes.lg};
 		font-family: ${props => props.theme.fontFamily.body};
 		font-weight: ${props => props.theme.fontWeights.bold};
-		margin-block: ${props => props.theme.spacing.xs};
+		margin-block: ${props => props.theme.spacing.sm};
 	}
 `;
 
 export const StyledVennResultDetailBody = styled.div`
+	ul {
+		margin-block: 0;
+		margin-inline: ${props => props.theme.spacing.md};
+		padding: 0;
+		
+		li {
+			button {
+				appearance: none;
+				border: 0;
+				padding: 0;
+				background: none;
+				cursor: pointer;
+				font-size: 1rem;
+				text-decoration: underline;
+				transition: all 0.2s ease-in-out;
+				
+				&:hover, &:focus, &:active {
+					color: ${props => props.theme.colors.info};
+				}
+			}
+		}
+	}
+	
 	p {
 		margin: 0;
 		line-height: 1.3;
